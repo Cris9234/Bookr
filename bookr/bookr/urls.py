be8933b9b99f6e63728da2e7c8f73d8fd1cc1f93
Cristiano Angeli <cristiano.a992@gmail.com>
2022-03-20 23:42:29 +0100

    - Create model Book - Create model Contributor - Create model Review - Create model BookContributor - Add field contributors to book - Add field publisher to book

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -19,5 +19,6 @@ import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', reviews.views.index)
+    path('', reviews.views.index),
+    path('bookr-search/', reviews.views.book_search)
 ]
--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    name = request.GET.get("name") or "world"
-    return HttpResponse("Hello, {}!".format(name))
+    title = "Welcome to Bookr"
+    welcome = "Welcome to Bookr"
+    name = "world"
+    return render(request, "base.html", {"title": title, "welcome": welcome, "name": name})
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-result.html", {"search_text": search_text})
