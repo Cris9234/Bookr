6f228d90d1a488c8224a11a935815b11e0e77ce0
cris <cristiano.a992@gmail.com>
2022-03-30 17:52:10 +0200

    change some settings

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,12 +1,3 @@
 from django.shortcuts import render
-
-
-def index(request):
-    title = "Welcome to Bookr"
-    welcome = "Welcome to Bookr"
-    name = "world"
-    return render(request, "base.html", {"title": title, "welcome": welcome, "name": name})
-
-def book_search(request):
-    search_text = request.GET.get("search", "")
-    return render(request, "search-result.html", {"search_text": search_text})
+from .models import  import Book, Review
+from .utils import import average_rating
