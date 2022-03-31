a99825829cbb1b0b1cc7c16aeffcdc4357f9397d
cris <cristiano.a992@gmail.com>
2022-03-31 16:26:59 +0200

    check the db settings!

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.welcome_view, name='welcome_view'), # non ho capito a che serve dargli un nome...
-]
+urlpatterns = [path('books/', views.book_list, name='book_list'), ]
