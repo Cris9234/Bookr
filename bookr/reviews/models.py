7ee87c986d4b0967998eae624a28a8f5dfb22d8b
A. Cristiano <cristiano.a992@gmail.com>
2022-04-03 23:58:37 +0200

    done some changes

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -56,11 +56,12 @@ class BookContributor(models.Model):
 
 class Review(models.Model):
     content = models.TextField(help_text="The Review text.")
-    rating = models.IntegerField(help_text="The rating the reviewer has given.")
+    rating = models.IntegerField(help_text="The the reviewer has given.")
     date_created = models.DateTimeField(auto_now_add=True,
                                         help_text="The date and time the review was created.")
     date_edited = models.DateTimeField(null=True,
-                                       help_text="The date and time the review was last edited.")
+                                       help_text='''The date and time the review was last edited.'''
+                                       )
     creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE,
                              help_text="The Book that this review is for.")
